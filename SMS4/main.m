07b27f9bde624920bd60a98923cf21593137b39b
Kassol <kassol.zx@gmail.com>
2015-03-02 21:11:43 +0800

    Initial Commit

---
+++ SMS4/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SMS4
+//
+//  Created by 张旭 on 15/3/2.
+//  Copyright (c) 2015年 3lang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
